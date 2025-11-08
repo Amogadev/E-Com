5f0153161b502f04429f717001b47f150a43b687
Amoga devi <amogadevi.s10@gmail.com>
2025-11-08 08:25:13 +0000

    Join Our Community of Success Real stories from sellers who built their

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
         <FeaturesSection />
         <SellingProcessSection />
         <StartSellingTodaySection />
-        <SuccessStoriesSection />
         <FooterCtaSection />
+        <SuccessStoriesSection />
       </main>
       <Footer />
     </div>
