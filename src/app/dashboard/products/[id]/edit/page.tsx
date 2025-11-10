6ab4803e1bcaa22b85be98c91acb456d5a8f980c
Amoga devi <amogadevi.s10@gmail.com>
2025-11-10 05:49:41 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '..

--- src/app/dashboard/products/[id]/edit/page.tsx
+++ src/app/dashboard/products/[id]/edit/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { products } from '../../../data';
+import { products } from '../../data';
 import { notFound, useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
